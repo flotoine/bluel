37023bc4d48ca4a5a79c8e213409d80820534ccf
Florian Antoine <florian.antoine.m@gmail.com>
2024-03-13 17:25:48 +0100

    connexion retourne erreur ou ok+token

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,22 +1,32 @@
-
-
-
-
 const formConnexion = document.querySelector('form');
-await formConnexion.addEventListener("submit", function(event) {
+
+formConnexion.addEventListener("submit", function(event) {
     event.preventDefault();
+    fetchConnection();
+});
+
+async function fetchConnection () {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     console.log(email);
     console.log(password);
-    const reponse = fetch("http://localhost:5678/api/users/login", {    /// compare email et mdp via API
+    const response = await fetch("http://localhost:5678/api/users/login", {    /// compare email et mdp via API
     method: 'POST',
     headers: { 
         "Content-Type": "application/json", 
-        "accept": "application/json"
     },
     body: `{ "email": "${email}", "password": "${password}"}`   /* '{ "email": "sophie.bluel@test.tld", "password": "S0phie"}'  */
     });
-    console.log(reponse); //promise pending ici il faut résoudre avec thens
-});
+
+
+    /// lecture réponse couple mail + mdp
+    if (response.ok) {  // plus de détail dans response.status (200, 401, 404)
+        console.log("ok");
+        const token = (await response.json()).token ///extraction du token
+        console.log(token) 
+    } else {
+        console.log("error")
+    }
+}
+
 
