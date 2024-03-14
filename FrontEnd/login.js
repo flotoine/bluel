c488d3f1579b63e0de72485966ebd7317c187846
Florian Antoine <florian.antoine.m@gmail.com>
2024-03-14 18:04:04 +0100

    mode edition: démarrage

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -21,12 +21,41 @@ async function fetchConnection () {
 
     /// lecture réponse couple mail + mdp
     if (response.ok) {  // plus de détail dans response.status (200, 401, 404)
-        console.log("ok");
-        const token = (await response.json()).token ///extraction du token
-        console.log(token) 
+        const token = (await response.json()).token; ///extraction du token
+        console.log(token);
+        window.location.href = "./index.html"
+        adminIndex()  // à conditionner à la connexion utilisatrice
     } else {
-        console.log("error")
+        connectionError();
     }
 }
 
 
+
+// fonction d'affichage du message d'erreur de connexion 
+function connectionError () {   
+    let errorDisplay = document.querySelector(".errorDisplay")
+    let errorMessage = document.createElement("p")
+    errorMessage.innerHTML = "Erreur d'authentification"
+    errorDisplay.appendChild(errorMessage)
+}
+
+
+
+
+
+
+
+/// ajoute bandeau mode d'édition pour l'admin + bouton modifier
+function adminIndex () {
+    let body = document.querySelector("body"); // va chercher le body de login alors que je veux celui de index
+    let editMenu = document.createElement("div");
+    editMenu.className = "editMenu"
+    editMenu.innerHTML = "<i class=\"fa-regular fa-pen-to-square\"></i>&nbspMode édition"
+    body.insertBefore(editMenu, body.children[0])
+    let editStyle = document.createElement("style") ;
+    editStyle.innerHTML = '.editMenu {display: flex; justify-content:center; align-items:center; background-color:black; color:white; height:40px; width: 100vw; position: fixed; top: 0px;left: calc(-50vw + 50%);}' ;
+    document.getElementsByTagName("head")[0].appendChild(editStyle) ;
+    console.log("fin admin index")
+
+}
